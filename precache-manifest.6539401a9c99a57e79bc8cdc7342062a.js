ca747c35a1d11d9834208dfa8bb0bb82242e23c6
Denis Loza <denisloza.git@gmail.com>
2021-04-28 19:02:40 +0300

    Updates

---
+++ precache-manifest.6539401a9c99a57e79bc8cdc7342062a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d753cfcfa13701b92ce53eae2b77aeeb",
+    "url": "/Counter_withRedux/index.html"
+  },
+  {
+    "revision": "dbf63bfcefcf619b94a6",
+    "url": "/Counter_withRedux/static/css/main.15f5d6c9.chunk.css"
+  },
+  {
+    "revision": "e07515eef90a64481c64",
+    "url": "/Counter_withRedux/static/js/2.f0055e4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Counter_withRedux/static/js/2.f0055e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf63bfcefcf619b94a6",
+    "url": "/Counter_withRedux/static/js/main.93d835cf.chunk.js"
+  },
+  {
+    "revision": "63c1791985e5816bef0d",
+    "url": "/Counter_withRedux/static/js/runtime-main.34dc999e.js"
+  }
+]);
